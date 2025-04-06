c0d654d8dd706bc79c51f5382e22c7755e1153fe
Sohail Akhtar Ali <alisohail2448@gmail.com>
2025-04-06 10:20:07 +0530

    Sohail | feat(dev): added drizzle config schema and readme changes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'drizzle-kit'
-
+import { defineConfig } from "drizzle-kit";
 export default defineConfig({
+  // schema: "./src/db/schema.ts",
+  // dbCredentials: {
+  //   connectionString: process.env.DATABASE_URL!,
+  // },
+  dialect: "postgresql",
   schema: "./src/db/schema.ts",
-  dialect: 'postgresql',
+  // driver: "pg",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
   verbose: true,
   strict: true,
-})
+});
